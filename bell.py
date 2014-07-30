b1ddd4973254f6cf1da94184a79033f90dc92d14
banasrini <bhavana@bhavanas-MacBook-Pro.local>
2014-07-30 12:24:06 -0700

    commit

--- bell.py
+++ bell.py
@@ -3,7 +3,7 @@ import time
 prev_input = 0
 while True:
   #take a reading
-  input = GPIO.input(17)
+  input = GPIO.input(24)
   #if the last reading was low and this one high, print
   if ((not prev_input) and input):
     print("Button pressed")
